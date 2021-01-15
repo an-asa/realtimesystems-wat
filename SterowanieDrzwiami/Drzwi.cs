f901ab13e716efe0b3c3aebdcba5871ce8ec6313
Jakub Krawczuk <kubkra@gmail.com>
2021-01-15 18:47:49 +0100

    Serwer Drzwi uruchomiony - brak komunikacji

---
+++ SterowanieDrzwiami/Drzwi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SterowanieDrzwiami
+{
+    public class Drzwi : IAsyncDisposable
+    {
+        public Drzwi()
+        {
+
+        }
+
+        public ValueTask DisposeAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+
+    }
+}
---
+++ SterowanieDrzwiami/DrzwiHub.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SterowanieDrzwiami
+{
+    public class DrzwiHub : Hub
+    {
+        Drzwi pDevice;
+        public DrzwiHub(Drzwi device)
+        {
+            pDevice = device;
+        }
+
+
+    }
+}
